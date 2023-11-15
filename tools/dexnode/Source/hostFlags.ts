2d5c2eec8a1b22e337d0d9392c32e48c65f4737b
Nikola Hristov <nikola@nikolahristov.tech>
2023-11-15 19:57:27 +0200

    squash!

--- tools/dexnode/Source/hostFlags.ts
+++ tools/dexnode/Source/hostFlags.ts
@@ -2,15 +2,15 @@
 // Licensed under the MIT License.
 
 export const enum HostFlags {
-	None = 0,
+    None = 0,
 
-	NodeJSArguments = 1 << 0,
-	DenoArguments = 2 << 0,
-	ChromiumArguments = 3 << 0,
-	ArgumentsMask = NodeJSArguments | DenoArguments | ChromiumArguments,
+    NodeJSArguments = 1 << 0,
+    DenoArguments = 2 << 0,
+    ChromiumArguments = 3 << 0,
+    ArgumentsMask = NodeJSArguments | DenoArguments | ChromiumArguments,
 
-	DetectV8Version = 1 << 2,
-	UseLatestV8Version = 1 << 3,
-	IsCurrent = 1 << 4,
-	UseNoLogfilePerIsolate = 1 << 5,
+    DetectV8Version = 1 << 2,
+    UseLatestV8Version = 1 << 3,
+    IsCurrent = 1 << 4,
+    UseNoLogfilePerIsolate = 1 << 5,
 }
